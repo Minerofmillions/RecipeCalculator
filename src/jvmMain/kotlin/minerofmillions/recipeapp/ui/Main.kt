a5a69fc32d147f3ae21b7b05a89ab509faa0d91d
MineMaster284 <jasoncollins818@gmail.com>
2022-07-13 11:19:43 -0400

    Update main UI

--- src/jvmMain/kotlin/minerofmillions/recipeapp/ui/Main.kt
+++ src/jvmMain/kotlin/minerofmillions/recipeapp/ui/Main.kt
@@ -1,14 +1,9 @@
 package minerofmillions.recipeapp.ui
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.VerticalScrollbar
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -26,41 +21,79 @@ fun App() {
 	val calculatorState = rememberCalculatorState()
 	MaterialTheme(colors = if (darkMode) darkColors() else lightColors()) {
 		Scaffold(topBar = {
-			TopAppBar(
-				actions = {
-					AppActions(calculatorState)
-					IconButton(onClick = { darkMode = !darkMode }) {
+			TopAppBar(actions = {
+				AppActions(calculatorState)
+				IconButton(onClick = { darkMode = !darkMode }) {
 //						Icon(Icons.Default.Favorite, if (darkMode) "Dark Mode" else "Light Mode")
-						if (darkMode) AsyncImage(
-							load = { loadSvgPainter(javaClass.getResource("/light_mode_white.svg"), density) },
-							painterFor = { it },
-							contentDescription = "Light Mode",
-						)
-						else AsyncImage(
-							load = { loadSvgPainter(javaClass.getResource("/dark_mode_white.svg"), density) },
-							painterFor = { it },
-							contentDescription = "Dark Mode",
-						)
-					}
-				},
-				title = {
-					Text("Recipe Calculator")
+					if (darkMode) AsyncImage(
+						load = { loadSvgPainter(javaClass.getResource("/light_mode_white.svg"), density) },
+						painterFor = { it },
+						contentDescription = "Light Mode",
+					)
+					else AsyncImage(
+						load = { loadSvgPainter(javaClass.getResource("/dark_mode_white.svg"), density) },
+						painterFor = { it },
+						contentDescription = "Dark Mode",
+					)
 				}
-			)
+			}, title = {
+				Text("Recipe Calculator")
+			})
 		}) { padding ->
 			Column(Modifier.padding(padding)) {
-				Row {
-					val state = rememberLazyListState()
-					LazyColumn(state = state, modifier = Modifier.weight(1f)) {
-						items(calculatorState.recipes) {
-							Column {
-								Text(it.name)
-								Text(it.inputs.joinToString())
-								Text(it.outputs.joinToString())
+				var tab by remember { mutableStateOf(0) }
+				TabRow(tab) {
+					Tab(tab == 0, onClick = { tab = 0 }, text = { Text("Recipes") })
+					Tab(tab == 1, onClick = { tab = 1 }, text = { Text("Items") })
+				}
+				when (tab) {
+					0 -> {
+						var searchTerm by remember { mutableStateOf("") }
+						val shownRecipes by remember {
+							derivedStateOf {
+								if (calculatorState.loadingRecipes) emptyList()
+								else calculatorState.recipes.filter { r ->
+									if (searchTerm.isEmpty()) true
+									else when {
+										searchTerm.startsWith("=>") -> r.outputs.any { it.item == searchTerm.substring(2) }
+										searchTerm.startsWith("<=") -> r.inputs.any { it.item == searchTerm.substring(2) }
+										searchTerm.startsWith('>') -> r.outputs.any { searchTerm.substring(1) in it.item }
+										searchTerm.startsWith('<') -> r.inputs.any { searchTerm.substring(1) in it.item }
+										else -> r.name.contains(searchTerm, true)
+									}
+								}
 							}
 						}
+						TextField(searchTerm,
+							{ searchTerm = it },
+							Modifier.fillMaxWidth(),
+							label = { Text("Search Recipes") })
+						ListView(shownRecipes) {
+							RecipeView(it)
+						}
 					}
-					VerticalScrollbar(rememberScrollbarAdapter(state))
+					1 -> {
+						var searchTerm by remember { mutableStateOf("") }
+						val shownItems by remember {
+							derivedStateOf {
+								if (calculatorState.loadingRecipes) emptyList()
+								else calculatorState.items.filter {
+									when {
+										searchTerm.startsWith('=') -> searchTerm.substring(1) == it.type.toString()
+										searchTerm.startsWith('@') -> searchTerm.substring(1) == it.mod
+										else -> Regex(searchTerm, RegexOption.IGNORE_CASE).containsMatchIn(it.name)
+									}
+								}
+							}
+						}
+						TextField(searchTerm,
+							{ searchTerm = it },
+							Modifier.fillMaxWidth(),
+							label = { Text("Search Items") })
+						ListView(shownItems) {
+							ItemView(it)
+						}
+					}
 				}
 			}
 		}
@@ -76,11 +109,15 @@ fun AppActions(state: CalculatorState) {
 		else Button(onClick = { state.clearRecipes() }) {
 			Text("Clear Recipes")
 		}
+	} else {
+		Button(onClick = { state.stopLoadingRecipes() }) {
+			Text("Stop Loading")
+		}
 	}
 }
 
 fun main() = application {
-	Window(onCloseRequest = ::exitApplication) {
+	Window(onCloseRequest = ::exitApplication, title = "Recipe Calculator") {
 		App()
 	}
 }
